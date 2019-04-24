ca9ad6f8ef5694556f810b13b01d18655ced55d4
anonymous <mattktechie@gmail.com>
2019-04-24 16:42:58 -0700

    Project Update 5

---
+++ FinalProject/ProjectUpdate5/Assets/Scripts/Level2Music.cs
@@ -0,0 +1,25 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level2Music : MonoBehaviour {
+	public GameObject _Player_control;
+	public float TotalTime;
+	public AudioClip MusicClip;
+	public AudioSource MusicSource;
+	// Use this for initialization
+	void Start () {
+		MusicSource.clip = MusicClip;
+		MusicSource.Play();
+		TotalTime = 60f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Text myTXT = GameObject.Find("Canvas/Text").GetComponent<Text>();
+		TotalTime -= Time.deltaTime;
+		myTXT.text = TotalTime.ToString() + " seconds";
+	}
+}
